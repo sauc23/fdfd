69def5752adc3dab3cad3f286b1b0bcf4ca2f593
Jaryl Chng <git@jarylchng.com>
2020-09-21 03:04:34 +0800

    enclosed manifest strings in quotes

--- manifest.sh
+++ manifest.sh
@@ -1,17 +1,17 @@
-#!/bin/sh
+#!/bin/ash
 
-echo ${TOKEN} | docker login -u ${USERNAME} --password-stdin ${REGISTRY}
-
-docker manifest create \
-  ${USERNAME}/${REGISTRY_IMAGE}:${1} \
-  -a ${USERNAME}/${REGISTRY_IMAGE}:amd64 \
-  -a ${USERNAME}/${REGISTRY_IMAGE}:armv7 \
-  -a ${USERNAME}/${REGISTRY_IMAGE}:arm64
-docker manifest push ${USERNAME}/${REGISTRY_IMAGE}:${1}
+echo "${TOKEN}" | docker login -u "${USERNAME}" --password-stdin "${REGISTRY}"
 
 docker manifest create \
-  ${USERNAME}/${REGISTRY_IMAGE}:latest \
-  -a ${USERNAME}/${REGISTRY_IMAGE}:amd64 \
-  -a ${USERNAME}/${REGISTRY_IMAGE}:armv7 \
-  -a ${USERNAME}/${REGISTRY_IMAGE}:arm64
-docker manifest push ${USERNAME}/${REGISTRY_IMAGE}:latest
+  "${USERNAME}/${REGISTRY_IMAGE}:${1}" \
+  -a "${USERNAME}/${REGISTRY_IMAGE}:amd64" \
+  -a "${USERNAME}/${REGISTRY_IMAGE}:armv7" \
+  -a "${USERNAME}/${REGISTRY_IMAGE}:arm64"
+docker manifest push "${USERNAME}/${REGISTRY_IMAGE}:${1}"
+
+docker manifest create \
+  "${USERNAME}/${REGISTRY_IMAGE}:latest" \
+  -a "${USERNAME}/${REGISTRY_IMAGE}:amd64" \
+  -a "${USERNAME}/${REGISTRY_IMAGE}:armv7" \
+  -a "${USERNAME}/${REGISTRY_IMAGE}:arm64"
+docker manifest push "${USERNAME}/${REGISTRY_IMAGE}:latest"
