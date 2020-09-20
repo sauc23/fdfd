8b5a38e6c19d198b7fda073bd15ed1ab120dc50e
Jaryl Chng <git@jarylchng.com>
2020-09-21 03:29:00 +0800

    removed '/' from manifest.sh

--- manifest.sh
+++ manifest.sh
@@ -5,7 +5,7 @@ echo "${TOKEN}" | docker login -u "${USERNAME}" --password-stdin ${REGISTRY}
 docker manifest create \
   "${REGISTRY_IMAGE}:${1}" \
   -a "${REGISTRY_IMAGE}:amd64" \
-  -a "${REGISTRY_IMAGE}/:armv7" \
+  -a "${REGISTRY_IMAGE}:armv7" \
   -a "${REGISTRY_IMAGE}:arm64"
 docker manifest push "${REGISTRY_IMAGE}:${1}"
 
