0e3f65cdc903def2dee680c1b4e42ef6d7afcce9
Jaryl Chng <git@jarylchng.com>
2020-09-21 02:05:57 +0800

    fix manifest.sh

--- manifest.sh
+++ manifest.sh
@@ -3,13 +3,13 @@
 docker manifest create \
   ${USERNAME}/${REGISTRY_IMAGE}:latest \
   -a ${USERNAME}/${REGISTRY_IMAGE}:amd64 \
-  -a ${USERNAME}/${REGISTRY_IMAGE}:arm32v7 \
-  -a ${USERNAME}/${REGISTRY_IMAGE}:arm64v8
+  -a ${USERNAME}/${REGISTRY_IMAGE}:armv7 \
+  -a ${USERNAME}/${REGISTRY_IMAGE}:arm64
 docker ${USERNAME}/${REGISTRY_IMAGE}:latest
 
 docker manifest create \
   ${USERNAME}/${REGISTRY_IMAGE}:${1} \
   -a ${USERNAME}/${REGISTRY_IMAGE}:amd64 \
-  -a ${USERNAME}/${REGISTRY_IMAGE}:arm32v7 \
-  -a ${USERNAME}/${REGISTRY_IMAGE}:arm64v8
+  -a ${USERNAME}/${REGISTRY_IMAGE}:armv7 \
+  -a ${USERNAME}/${REGISTRY_IMAGE}:arm64
 docker manifest push ${USERNAME}/${REGISTRY_IMAGE}:${1}
