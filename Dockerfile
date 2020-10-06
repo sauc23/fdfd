db5dbe5e23373977d3b202901f7b4284023da988
Jaryl Chng <git@jarylchng.com>
2020-10-07 03:27:36 +0800

    move env to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,15 @@ RUN apk add yarn python3 build-base && \
     rm -r .git .npmignore package*
 
 FROM alpine
+ENV HTTP=1 \
+    UDP=1 \
+    WS=1 \
+    QUIET=1 \
+    SILENT=0 \
+    TRUST_PROXY=0 \
+    STATS=1 \
+    INTERVAL=600000 \
+    PORT=8000
 RUN apk add npm
 WORKDIR /home/node/
 COPY --from=builder /home/node/ .
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,18 @@
 npm audit fix
 
 p=""
-[[ "${HTTP:-1}" == 1 ]] && p=$p'--http '
-[[ "${UDP:-1}" -eq 1 ]] && p=$p'--udp '
-[[ "${WS:-1}" -eq 1 ]] && p=$p'--ws '
-if [[ "${SILENT:-0}" -eq 0 ]]; then
-  [[ "${QUIET:-1}" -eq 1 ]] && p=$p'-q '
+[[ "${HTTP}" == 1 ]] && p=$p'--http '
+[[ "${UDP}" -eq 1 ]] && p=$p'--udp '
+[[ "${WS}" -eq 1 ]] && p=$p'--ws '
+if [[ "${SILENT}" -eq 0 ]]; then
+  [[ "${QUIET}" -eq 1 ]] && p=$p'-q '
 else
   p=$p'-s '
 fi
-p=$p"--trust-proxy $([[ ${TRUST_PROXY:-0} -eq 1 ]] && echo true || echo false) "
-p=$p"--stats $([[ ${STATS:-1} -eq 1 ]] && echo true || echo false) "
-p=$p"--interval ${INTERVAL:-600000} "
-p=$p"-p ${PORT:-8000}"
+p=$p"--trust-proxy $([[ ${TRUST_PROXY} -eq 1 ]] && echo true || echo false) "
+p=$p"--stats $([[ ${STATS} -eq 1 ]] && echo true || echo false) "
+p=$p"--interval ${INTERVAL} "
+p=$p"-p ${PORT}"
 
 echo "Parameters: ${p}"
 NODE_ENV="production" bin/cmd.js ${p}
