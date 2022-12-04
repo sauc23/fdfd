9cd8b4f3eea4372d4b4b251e27a0638bd0b473b6
Jaryl Chng <git@jarylchng.com>
2022-12-04 11:25:47 +0800

    update build script

--- build.sh
+++ build.sh
@@ -2,13 +2,16 @@
 
 apk add curl jq
 
+RUNNER_ARCH=$(arch)
+RUNNER_ARCH=${RUNNER_ARCH/x86_/amd}
+RUNNER_ARCH=${RUNNER_ARCH/aarch/arm}
 BUILDX_VER=$(curl -ks https://api.github.com/repos/docker/buildx/releases/latest | jq -r '.name')
 mkdir -p "$HOME/.docker/cli-plugins/"
-wget -O "$HOME/.docker/cli-plugins/docker-buildx" "https://github.com/docker/buildx/releases/download/${BUILDX_VER}/buildx-${BUILDX_VER}.linux-amd64"
+wget -O "$HOME/.docker/cli-plugins/docker-buildx" "https://github.com/docker/buildx/releases/download/${BUILDX_VER}/buildx-${BUILDX_VER}.linux-${RUNNER_ARCH}"
 chmod a+x "$HOME/.docker/cli-plugins/docker-buildx"
 echo -e '{\n  "experimental": "enabled"\n}' | tee "$HOME/.docker/config.json"
 
-if [[ ${ARCH} != "amd64" ]]; then
+if [[ ${RUNNER_ARCH} != ${ARCH} ]]; then
   docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 fi
 
