98ff9c6b67c61f93bcd7ead87dec42d53e82ef22
Jaryl Chng <git@jarylchng.com>
2023-01-16 16:52:25 +0800

    use docker buildx imagetools instead of docker manifest

--- manifest.sh
+++ manifest.sh
@@ -1,29 +1,34 @@
 #!/bin/ash
 
+RUNNER_ARCH=$(arch)
+RUNNER_ARCH=${RUNNER_ARCH/x86_/amd}
+RUNNER_ARCH=${RUNNER_ARCH/aarch/arm}
+BUILDX_VER=$(curl -ks https://api.github.com/repos/docker/buildx/releases/latest | jq -r '.name')
+mkdir -p "$HOME/.docker/cli-plugins/"
+wget -O "$HOME/.docker/cli-plugins/docker-buildx" "https://github.com/docker/buildx/releases/download/${BUILDX_VER}/buildx-${BUILDX_VER}.linux-${RUNNER_ARCH}"
+chmod a+x "$HOME/.docker/cli-plugins/docker-buildx"
+echo -e '{\n  "experimental": "enabled"\n}' | tee "$HOME/.docker/config.json"
+
 echo "${TOKEN}" | docker login -u "${USERNAME}" --password-stdin ${REGISTRY}
 
-docker manifest create \
-  "${REGISTRY_IMAGE}:${1}" \
-  -a "${REGISTRY_IMAGE}:amd64" \
-  -a "${REGISTRY_IMAGE}:armv7" \
-  -a "${REGISTRY_IMAGE}:arm64"
-docker manifest push "${REGISTRY_IMAGE}:${1}"
-docker manifest create \
-  "${REGISTRY_IMAGE}:latest" \
-  -a "${REGISTRY_IMAGE}:amd64" \
-  -a "${REGISTRY_IMAGE}:armv7" \
-  -a "${REGISTRY_IMAGE}:arm64"
-docker manifest push "${REGISTRY_IMAGE}:latest"
+docker buildx imagetools create \
+  -t "${REGISTRY_IMAGE}:${1}" \
+  "${REGISTRY_IMAGE}:amd64" \
+  "${REGISTRY_IMAGE}:armv7" \
+  "${REGISTRY_IMAGE}:arm64"
+docker buildx imagetools create \
+  -t "${REGISTRY_IMAGE}:latest" \
+  "${REGISTRY_IMAGE}:amd64" \
+  "${REGISTRY_IMAGE}:armv7" \
+  "${REGISTRY_IMAGE}:arm64"
 
-docker manifest create \
-  "${REGISTRY_IMAGE2}:${1}" \
-  -a "${REGISTRY_IMAGE}:amd64" \
-  -a "${REGISTRY_IMAGE}:armv7" \
-  -a "${REGISTRY_IMAGE}:arm64"
-docker manifest push "${REGISTRY_IMAGE2}:${1}"
-docker manifest create \
-  "${REGISTRY_IMAGE2}:latest" \
-  -a "${REGISTRY_IMAGE}:amd64" \
-  -a "${REGISTRY_IMAGE}:armv7" \
-  -a "${REGISTRY_IMAGE}:arm64"
-docker manifest push "${REGISTRY_IMAGE2}:latest"
+docker buildx imagetools create \
+  -t "${REGISTRY_IMAGE2}:${1}" \
+  "${REGISTRY_IMAGE}:amd64" \
+  "${REGISTRY_IMAGE}:armv7" \
+  "${REGISTRY_IMAGE}:arm64"
+docker buildx imagetools create \
+  -t "${REGISTRY_IMAGE2}:latest" \
+  "${REGISTRY_IMAGE}:amd64" \
+  "${REGISTRY_IMAGE}:armv7" \
+  "${REGISTRY_IMAGE}:arm64"
